27731f64042ad6a0260ed85a7d2dfdfd6a423ce9
Roman Zipp <ich@ich.wtf>
2025-11-24 23:43:29 +0100

    Adjust sizes for small viewport

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
         heading: ['Work Sans', 'system-ui', '-apple-system', 'sans-serif'],
       },
+      screens: {
+        'xs': '306px',
+      },
     },
   },
   plugins: [],
